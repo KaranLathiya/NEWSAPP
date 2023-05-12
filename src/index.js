4e20f50dc09c28ac46c9eeb978818dc484cdab0f
KaranLathiya <123564272+KaranLathiya@users.noreply.github.com>
2023-05-12 20:40:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  <HashRouter Basenmae={process.env.PUBLIC_URL}>
     <App />
+  <HashRouter>
   // </React.StrictMode>
 );
 
